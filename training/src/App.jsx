851d8d11d1596568895e40c8e8ceb534c8af7d9f
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2021-01-27 22:59:08 +0530

    feat:Material UI - Styling, Dialog, and Form Validation

--- training/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TextFieldDemo } from './pages';
-
-const App = () => (
-  <TextFieldDemo />
-);
-
-export default App;
--- training/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { TextField } from './TextField';
-
-export { Slider } from './Slider';
