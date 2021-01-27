23ab62e042f164023e44ac2e0f0d050c24e43120
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2021-01-27 15:33:44 +0530

    feat:Children and Material UI Setup

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { SelectField } from './SelectField';
 export { RadioField } from './RadioField';
 export { Slider } from './Slider';
 export { ButtonField } from './Button';
+export { Math } from './Math';
